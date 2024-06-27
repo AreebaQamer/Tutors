bcf6d6625d16ecf6cf7b4b02878056c1ffc75c59
Areeba Qamer <115891143+AreebaQamer@users.noreply.github.com>
2024-06-28 00:53:06 +0500

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using E_Learning_Project_final.Models;
+using E_Learning_Project_final.Data.Migrations;
+
+namespace E_Learning_Project_final.Data
+{
+	public class ApplicationDbContext : IdentityDbContext<MyUser>
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+
+    }
+}
